584695c1c47c3dd3720ae1c964ed1c7de65ff6d3
Vincent Schouten <inquiry@intoreflection.co>
2021-06-01 22:59:39 +0200

    Reword list of keywords for PyPi

--- execute_cli.py
+++ execute_cli.py
@@ -7,5 +7,5 @@ from powermolecli.powermolecli import main
 if __name__ == '__main__':
     main()
 
-# issue ". powermolecli/.venv/bin/activate" in directory where both packages (cli and lib) reside
-# issue "python execute_cli.py -c /home/vincent/demo_2_hosts_for_mode.json -L debug"
+# 1. issue ". powermolecli/.venv/bin/activate" in directory where both packages (cli and lib) reside
+# 2. issue "python execute_cli.py -c /home/vincent/demo_2_hosts_for_mode.json -L debug"
